70881621cec356015e4e11a3f9f5f5a123499bf6
Антон <strider-13@mail.ru>
2018-06-16 23:09:55 +1000

    feat: add Ingredient class with Parcelable implementation and gson annotations

---
+++ app/src/main/java/com/viatorfortis/bebaker/model/Ingredient.java
@@ -0,0 +1,54 @@
+package com.viatorfortis.bebaker.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient implements Parcelable {
+
+    @SerializedName("ingredient")
+    private final String mIngredient;
+
+    @SerializedName("measure")
+    private final String mMeasure;
+
+    @SerializedName("quantity")
+    private final float mQuantity;
+
+    public Ingredient(String ingredient, String measure, float quantity) {
+        mIngredient = ingredient;
+        mMeasure = measure;
+        mQuantity = quantity;
+    }
+
+    private Ingredient(Parcel parcel) {
+        mIngredient = parcel.readString();
+        mMeasure = parcel.readString();
+        mQuantity = parcel.readFloat();
+    }
+
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel parcel) {
+            return new Ingredient(parcel);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mIngredient);
+        parcel.writeString(mMeasure);
+        parcel.writeFloat(mQuantity);
+    }
+}
--- app/src/main/java/com/viatorfortis/bebaker/ui/MainActivity.java
+++ app/src/main/java/com/viatorfortis/bebaker/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.viatorfortis.bebaker;
+package com.viatorfortis.bebaker.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.viatorfortis.bebaker.R;
 
 public class MainActivity extends AppCompatActivity {
 
