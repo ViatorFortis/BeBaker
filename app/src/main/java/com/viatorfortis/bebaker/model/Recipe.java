36f0b11bb3b5a2be3a6dd6eaa8a1fdfa81271e6d
Антон <strider-13@mail.ru>
2018-06-18 00:13:54 +1000

    feat: add Recipe class with Parcelable implementation and gson annotations

---
+++ app/src/main/java/com/viatorfortis/bebaker/model/Recipe.java
@@ -0,0 +1,80 @@
+package com.viatorfortis.bebaker.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Recipe
+        implements Parcelable {
+
+    @SerializedName("id")
+    private int mId;
+
+    @SerializedName("name")
+    private String mName;
+
+    @SerializedName("ingredients")
+    private List<Ingredient> mIngredientList;
+
+    @SerializedName("steps")
+    private List<Step> mStepList;
+
+    public int getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public List<Ingredient> getIngredientList() {
+        return mIngredientList;
+    }
+
+    public List<Step> getStepList() {
+        return mStepList;
+    }
+
+    public Recipe(int id, String name, List<Ingredient> ingredientList, List<Step> stepList) {
+        mId = id;
+        mName = name;
+        mIngredientList = ingredientList;
+        mStepList = stepList;
+    }
+
+    private Recipe(Parcel parcel) {
+        mId = parcel.readInt();
+        mName = parcel.readString();
+        parcel.readList(mIngredientList, Ingredient.class.getClassLoader() );
+        parcel.readList(mStepList, Step.class.getClassLoader() );
+    }
+
+
+    public static final Parcelable.Creator<Recipe> CREATOR = new Parcelable.Creator<Recipe> () {
+        @Override
+        public Recipe createFromParcel(Parcel parcel) {
+            return new Recipe(parcel);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(mId);
+        parcel.writeString(mName);
+        parcel.writeList(mIngredientList);
+        parcel.writeList(mStepList);
+    }
+}
