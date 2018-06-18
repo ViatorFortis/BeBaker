370965b2b075a666fdf45f2c2da2bf8d8d04f6d7
Антон <strider-13@mail.ru>
2018-06-18 21:49:51 +1000

    feat: add JsonUtils class for recipe list parsing

---
+++ app/src/main/java/com/viatorfortis/bebaker/utilities/JsonUtils.java
@@ -0,0 +1,21 @@
+package com.viatorfortis.bebaker.utilities;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.viatorfortis.bebaker.model.Recipe;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class JsonUtils {
+    public static List<Recipe> parseRecipeListJson(String jsonArray)
+            throws JsonSyntaxException {
+        Type listType = new TypeToken<ArrayList<Recipe>>(){}.getType();
+        List<Recipe> recipeList = new Gson().fromJson(jsonArray, listType);
+
+        return recipeList;
+    }
+}
